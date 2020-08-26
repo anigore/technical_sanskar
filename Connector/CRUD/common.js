8d9a2a974682c56c7ab58c002328dc263bbab1ab
Aniket Gore <aniket@marensolutions.com>
2020-08-26 10:12:12 +0530

    common messages and status codes

--- Connector/CRUD/common.js
+++ Connector/CRUD/common.js
@@ -7,8 +7,17 @@ module.exports = {
         }
     },
 
-    SAVED_SUCCESS: "data saved successfully",
-    SAVED_ERROR: "",
+    /* *** MESSAGES *** */
 
-    
+    SAVED_SUCCESS: "data saved successfully",
+    SAVED_ERROR: "error in saving data",
+    NEWID_ERROR: "error while creating new Id",
+
+    /* *** STATUS CODES *** */
+
+    SUCCESS: 1000,
+    ERROR: 1001,
+    NOT_FOUND: 2001
+
+
 }
